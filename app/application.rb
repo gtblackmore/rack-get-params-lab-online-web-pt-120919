af05002c8fa7836c14eb535eeef3846673a12c3c
George Blackmore <gtblackmore@gmail.com>
2020-03-07 22:37:56 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -27,7 +27,7 @@ class Application
       if @@items.include? item
         @@cart << item
       else
-        
+        resp.write "We don't have that item"
     else
       resp.write "Path Not Found"
     end
